dc8e0cf17490e5b3d5725476bd07e27e5c2465a4
east9 <pinkeman@naver.com>
2019-08-22 17:27:43 +0900

    add env load resolve

--- src/server.js
+++ src/server.js
@@ -1,7 +1,4 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
-
+import "./env";
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import passport from "passport";
@@ -13,7 +10,7 @@ const PORT = process.env.PORT || 4000;
 const server = new GraphQLServer({ schema });
 
 server.express.use(logger("dev"));
-server.express.use(passport.authenticate("jwt"));
+
 server.start({ port: PORT }, () =>
 	console.log(`Server running on  http://localhost:${PORT}`),
 );
