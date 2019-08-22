241cd96cd47c17c606281e70db405d3a0aca7134
east9 <pinkeman@naver.com>
2019-08-22 17:10:15 +0900

    add func passport

---
+++ src/passport.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+import passport from "passport";
+import { Strategy, ExtractJwt } from "passport-jwt";
+import { prisma } from "../generated/prisma-client";
+
+const jwtOptions = {
+	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+	secretOrKey: process.env.JWT_SECRET,
+};
+
+const verifyUser = async (payload, done) => {
+	try {
+		const user = await prisma.user({ id: payload.id });
+		if (user !== null) {
+			return done(null, user);
+		} else {
+			return done(null, false);
+		}
+	} catch (error) {
+		return done(error, false);
+	}
+};
+
+passport.use(new Strategy(jwtOptions, verifyUser));
