38ebadb3aa69ddb53486dff63cb8219f523c4da7
east9 <pinkeman@naver.com>
2019-08-26 14:43:53 +0900

    modify: Please enter a charcter to search

--- src/api/User/searchUser/searchUser.js
+++ src/api/User/searchUser/searchUser.js
@@ -2,15 +2,22 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
 	Query: {
-		searchUser: async (_, args) =>
-			prisma.users({
-				where: {
-					OR: [
-						{ username_contains: args.term },
-						{ firstName_contains: args.term },
-						{ lastName_contains: args.term },
-					],
-				},
-			}),
+		searchUser: async (_, args) => {
+			const { term } = args;
+			if (term.length > 0) {
+				const users = await prisma.users({
+					where: {
+						OR: [
+							{ username_contains: args.term },
+							{ firstName_contains: args.term },
+							{ lastName_contains: args.term },
+						],
+					},
+				});
+				return users;
+			} else {
+				throw Error("Please enter a charcter to search");
+			}
+		},
 	},
 };
