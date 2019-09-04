441eef6ea596693076c232d0186ddcae1ab1265d
east9 <pinkeman@naver.com>
2019-09-04 17:02:48 +0900

    modify: for seeFullpost

--- src/fragment.js
+++ src/fragment.js
@@ -1,17 +1,9 @@
-export const USER_FRAGMENT = `
-    fragment UserParts on User{
+export const COMMENT_FRAGMENT = `
+    fragment CommentParts on Comment{
         id
-        username
-        email
-        firstName
-        lastName
-        bio
-        following{
-            id
-        }
-        posts{
-            id
-            caption
+        text
+        user{
+            username
         }
     }
 `;
