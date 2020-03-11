affe839641388aa9ccc398158262d4b0ba736cd5
kindunq <humanb2box@gmail.com>
2020-03-11 16:03:34 +0900

    fix: throw error exist user and email

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,6 +4,14 @@ export default {
 	Mutation: {
 		createAccount: async (_, args) => {
 			const { username, email, firstName = '', lastName = '', bio = '' } = args;
+			const existName = await prisma.$exists.user({ username: username });
+			const existEmail = await prisma.$exists.user({ email: email });
+			if (existName) {
+				throw Error('같은 이름의 사용자가 존재합니다.');
+			}
+			if (existEmail) {
+				throw Error('같은 이름의 이메일이 존재합니다.');
+			}
 			await prisma.createUser({
 				username,
 				email,
