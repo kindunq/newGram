46373a7d5900665b79713f9a71e35129899fbff9
kindunq <humanb2box@gmail.com>
2020-03-18 15:47:37 +0900

    update source

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,15 +4,17 @@ export default {
 	Mutation: {
 		createAccount: async (_, args) => {
 			const { username, email, firstName = '', lastName = '', bio = '' } = args;
-			const exist = await prisma.$exists.user({
-				OR: [{ username: username }, { email: email }]
+			const exists = await prisma.$exists.user({
+				OR: [
+					{
+						username
+					},
+					{ email }
+				]
 			});
-
-			if (exist) {
-				//throw Error('이름 / 이메일이 사용중입니다.');
-				return false;
+			if (exists) {
+				throw Error('이름 / 이메일이 사용중입니다.');
 			}
-
 			await prisma.createUser({
 				username,
 				email,
